a72614f81cc054e2b8d3187813b2c3d4f72e10ec
SloudPL <slowik432@gmail.com>
2023-03-24 18:27:43 +0100

    First version

--- src/main/java/pl/sloudpl/noplugincommand/Events.java
+++ src/main/java/pl/sloudpl/noplugincommand/Events.java
@@ -1,2 +1,41 @@
-package pl.sloudpl.noplugincommand;public class Events {
+package pl.sloudpl.noplugincommand;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+public class Events implements Listener {
+
+    NoPluginCommand plugin;
+
+    public Events(NoPluginCommand m){
+
+        plugin = m;
+
+    }
+
+    @EventHandler
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent e) {
+        Player p = e.getPlayer();
+        String cmd = e.getMessage();
+        if(!p.isOp() && !p.hasPermission("sloudpl.plugins")) {
+            if (cmd.contains("/pl")
+                    || cmd.contains("/plugins")
+                    || cmd.contains("/bukkit:pl")
+                    || cmd.contains("/bukkit:plugins")
+                    || cmd.contains("/ver")
+                    || cmd.contains("/bukkit:ver")
+                    || cmd.contains("/minecraft:me")
+                    || cmd.contains("/me")
+                    || cmd.contains("/?")
+                    || cmd.contains("/bukkit:?")
+                    || cmd.contains("/help")
+                    || cmd.contains("/bukkit:help")) {
+                e.setCancelled(true);
+                p.sendMessage("§cYou are not allowed to use this command!");
+                return;
+            }
+        }
+    }
 }
--- src/main/java/pl/sloudpl/noplugincommand/NoPluginCommand.java
+++ src/main/java/pl/sloudpl/noplugincommand/NoPluginCommand.java
@@ -4,14 +4,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NoPluginCommand extends JavaPlugin {
 
+    public static NoPluginCommand main;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+        main = this;
+
+        getServer().getPluginManager().registerEvents(new Events(this), this);
 
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public static NoPluginCommand getMain() {
+
+        return main;
+
     }
+
 }
